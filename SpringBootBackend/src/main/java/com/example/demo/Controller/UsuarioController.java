c29953c78d232a0cf5ec7ab2347c353c42e47f14
bryanhancco <bhanccoco@unsa.edu.pe>
2024-03-16 11:23:17 -0500

    Modificacion de CORS por controlador a CORS global

--- SpringBootBackend/src/main/java/com/example/demo/Controller/UsuarioController.java
+++ SpringBootBackend/src/main/java/com/example/demo/Controller/UsuarioController.java
@@ -14,35 +14,30 @@ import java.util.List;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<Usuario>> listarTodosLosUsuarios() {
         List<Usuario> usuarios = usuarioService.listarUsuarios();
         return new ResponseEntity<>(usuarios, HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id) {
         Usuario usuario = usuarioService.obtenerUsuario(id);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping
     public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) {
         Usuario nuevoUsuario = usuarioService.guardarUsuario(usuario);
         return new ResponseEntity<>(nuevoUsuario, HttpStatus.CREATED);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/{id}")
     public ResponseEntity<Void> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioActualizado) {
         usuarioService.actualizarUsuario(id, usuarioActualizado); // Llama al método de servicio para actualizar el usuario
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
         usuarioService.eliminarUsuario(id);
--- SpringBootBackend/src/main/java/com/example/demo/DemoApplication.java
+++ SpringBootBackend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	String angularUrl = "http://localhost:4200";
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//registry.addMapping("/api/usuarios").allowedOrigins(angularUrl);
+				registry.addMapping("/api/usuarios/**")
+					.allowedOrigins(angularUrl)
+					.allowedMethods("POST", "GET", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
